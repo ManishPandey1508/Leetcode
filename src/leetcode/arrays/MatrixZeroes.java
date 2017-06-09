21eb7786b2e5f6d3bbbd158118657ffaf2d70bc2
manish <manish.pandey@sjsu.edu>
2017-06-08 19:55:29 -0700

    73. Set Matrix Zeroes

---
+++ src/leetcode/arrays/MatrixZeroes.java
@@ -0,0 +1,63 @@
+package leetcode.arrays;
+
+public class MatrixZeroes {
+
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 1, 1, 0 }, { 1, 1, 1, 0 }, { 1, 1, 0, 0 }, { 1, 0, 0, 0 } };
+		new MatrixZeroes().new Solution().setZeroes(matrix);
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				
+				System.out.println(matrix[i][j]);
+				
+			}
+		}
+
+	}
+
+	class Solution {
+		public void setZeroes(int[][] matrix) {
+
+			boolean fRow = false, fCol = false;
+
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[0].length; j++) {
+
+					if (matrix[i][j] == 0) {
+						if (i == 0)
+							fRow = true;
+						if (j == 0)
+							fCol = true;
+						matrix[0][j] = 0;
+						matrix[i][0] = 0;
+					}
+
+				}
+			}
+
+			for (int i = 1; i < matrix.length; i++) {
+				for (int j = 1; j < matrix[0].length; j++) {
+
+					if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+						matrix[i][j] = 0;
+					}
+
+				}
+			}
+
+			if (fRow == true) {
+				for (int i = 0; i < matrix[0].length; i++) {
+
+					matrix[0][i] = 0;
+				}
+			}
+			if (fCol == true) {
+				for (int i = 0; i < matrix.length; i++) {
+					matrix[i][0] = 0;
+				}
+			}
+
+		}
+	}
+}
